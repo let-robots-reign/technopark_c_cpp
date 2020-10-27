5d50ace7282fac5ebf04ea157f13e99ebdb80a35
Alexey Zotov <zotov.alexei.1@yandex.ru>
2020-10-27 19:21:24 +0300

    reorganize project

--- hw2_static/include/count_comments.h
+++
@@ -1,9 +0,0 @@
-#ifndef TECHNOPARK_C_CPP_COUNT_COMMENTS_H
-#define TECHNOPARK_C_CPP_COUNT_COMMENTS_H
-
-#include <stdio.h>
-#include "comment_struct.h"
-
-int count_zero_votes_comments(const comment *comments, size_t size);
-
-#endif //TECHNOPARK_C_CPP_COUNT_COMMENTS_H
